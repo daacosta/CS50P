d1bb98083170de235156c4d17dd87ad196222b35
Diego A. Acosta <daacosta@gmail.com>
2024-07-28 07:34:24 -0500

    Just defined the last functio...

--- Plate.py
+++ Plate.py
@@ -5,7 +5,7 @@ Alphabet_lower = "".join(Alphabetlist)
 Alphabet_upper = Alphabet_lower.upper()
 Alphabet_complete = Alphabet_lower + Alphabet_upper
 Alphabet = set(Alphabet_complete)
-Numbers = set([x for x in range(0, 10)])
+Numbers = str(set([x for x in range(0, 10)]))
 Punctuation = string.punctuation
 Punctuation_set = set([ x for x in Punctuation])
 
@@ -64,6 +64,23 @@ def first_digit_not_zero(s):
     else:
         d = False
     return d
+
+#I need to verify that all digits are together starting from the very last index (-1).
+#So, I need to use the number of digits to have the indexes of i from -1 to -digits-1
+#changing by -1 every time... This is cool...
+
+def digits_in_last_places_continuous(s):
+    if first_digit_not_zero(s):
+        for i in range(-1, -count_digits(s)-1, -1):
+            if s[i] in Numbers:
+                d = True
+            else:
+                d = False
+                break
+            d = False
+    else:
+        d = False
+    return d
     
 print("Plate length between 2 and 6 characters?", plate_length(plate))
 print("First two characters are letters?", first_two_characters(plate))
@@ -71,3 +88,4 @@ print("No punctuation symbols?", no_punctuation_symbols(plate))
 print("How many digits are in the plate?", count_digits(plate))
 print("String's length is:", len(plate))
 print("The first digit is not a zero?", first_digit_not_zero(plate))
+print("All digits correctly placed?", digits_in_last_places_continuous(plate))
