22365f7c13fc1b90966e1d9a494e342e162b6f38
Diego A. Acosta <daacosta@gmail.com>
2024-08-01 21:28:40 -0500

    I did it!

--- grocery.py
+++ grocery.py
@@ -1,9 +1,21 @@
-lst = []
+list_raw = []
 
 try:
     while True:
         item = input()
-        lst.append(item)
+        list_raw.append(item)
 except EOFError:
     pass
-    print(lst)
+
+caps_list = []
+
+for item in list_raw:
+    item = item.upper()
+    caps_list.append(item)
+
+grocery_dict = {i:caps_list.count(i) for i in caps_list}
+
+items_list = sorted(list(grocery_dict.keys()))
+
+for i in range(len(items_list)):
+    print(grocery_dict.get(items_list[i]),items_list[i])
