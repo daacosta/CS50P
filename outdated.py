6cbbb5056ee01b66f266ccee9913b4fa1eddd2cb
Diego A. Acosta <daacosta@gmail.com>
2024-08-10 22:40:49 -0500

    Some progress... I am filtering what's good

--- outdated.py
+++ outdated.py
@@ -13,30 +13,38 @@ months = [
     "December"
 ]
 
-# month = input("Month: ")
-# index = months.index(month)
-# print(index + 1)
-
 # The anno_domini comes in two formats: (1) Complete Month name-Space-Number-Comma-Space-Four digit Number and (2) Nomber-Slash-Number-Slash-four digit number
 
 # The second number is between 1 and 31, No restrictions for the third number, the first number goes between 1 and 12
 
 # Approach 1: Work on the format that doesn't have any strings and split the MM/DD/YYYY format to convert it into YYY-MM-DD format.  Make it work fine from the very get go...  Convert the format Month Day, Year into the format with slashes and take it from there.
+anno_Domini = input("Date: ")
 
-anno_Domini = "September 8, 1636".split(",")
-print(anno_Domini)
-month_day = anno_Domini[0].split(" ")
-month_day.append(anno_Domini[1].strip())
+#https://www.geeksforgeeks.org/python-split-multiple-characters-from-string/
 
-List_anno_Domini = []
-List_anno_Domini.append(str(months.index(month_day[0])+1))
-List_anno_Domini.append(month_day[1])
-List_anno_Domini.append(month_day[2])
+delimiters = [",", " ", "/"]
+ 
+for delimiter in delimiters:
+    anno_Domini = " ".join(anno_Domini.split(delimiter))
+ 
+result = anno_Domini.split()
+ 
+print(result)
+print(result[0] in months)
 
-Formatted_as_numbers_only = "/".join(List_anno_Domini)
+try:
+    if result[0] in months:
+        result[0] = str(months.index(result[0]) + 1)
+    elif type(int(result[0])) is int:
+        pass
 
-print(Formatted_as_numbers_only)
+except ValueError:
+    print("Call main function again")
 
-print(List_anno_Domini)
-print(month_day)
-
+if int(result[0]) > 12 or int(result[0]) < 1:
+    print("Call main function again")
+elif int(result[1]) > 31 or int(result[1]) < 1:
+    print("Call main function again")
+else:
+    rearranged = [result[2], result[0], result[1]]
+    print(rearranged)
